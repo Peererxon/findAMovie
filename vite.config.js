004d3fd8b747aafd6cbbe4f8f5f14e2e66acf3a6
anderson gil <andersongil.dev@gmail.com>
2023-03-25 14:48:26 -0400

    added new page to see more info about a movie

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        info: resolve(__dirname, "src/info/index.html"),
       },
     },
   },
