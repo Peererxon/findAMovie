87cbc7b8036ff21617775618551a8b2ed3afa40d
Anderson Gil <andersongil.dev@gmail.com>
2023-04-03 14:33:08 -0400

    end sign up logic

---
+++ src/js/AuthService.js
@@ -0,0 +1,53 @@
+import { uuidv4 } from "./utils";
+
+export default class AuthService {
+  static getUsers() {
+    const items = localStorage.getItem("users");
+    return JSON.parse(items);
+  }
+  static signUpUser({ email, password }) {
+    const users = this.getUsers();
+
+    // This if is for when not users exist
+    if (!users) {
+      const uuid = uuidv4();
+      localStorage.setItem(
+        "users",
+        JSON.stringify([{ uuid, email, password }])
+      );
+      alert(`Not users found`);
+      return;
+    }
+
+    const userExists = this.checkIfExists(email);
+
+    // This is for when user already exists
+    if (userExists === true) {
+      alert("User already exists, please sign in");
+      return;
+    }
+
+    const uuid = uuidv4();
+
+    users.push({ uuid, email, password });
+
+    localStorage.setItem("users", JSON.stringify(users));
+
+    alert(`si existen usuarios, pero el email no esta registrado`);
+  }
+  static checkIfExists(email) {
+    const users = this.getUsers();
+
+    if (users) {
+      const match = users.find((user) => user.email === email);
+
+      if (match) {
+        return true;
+      }
+
+      return false;
+    } else {
+      return false;
+    }
+  }
+}
